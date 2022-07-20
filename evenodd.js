b8b31b66c6cd94a948db62c714936083262cd33e
mokashi21 <101655816+mokashi21@users.noreply.github.com>
2022-07-20 23:53:27 +0530

    Add files via upload

---
+++ evenodd.js
@@ -0,0 +1,15 @@
+
+// Program to check if a number is odd or even (Submittable assignment) 
+// using if else 
+    var number = 20;
+    if (number % 2 == 0) {
+        console.log("The number is Even");
+}
+    else 
+    console.log("The Number is odd")
+    console.log(number);
+
+    // here we declarerd a variable with name as number and given a value on line 4 &
+    // using if else formula we ve declared a number as odd or even,
+    // we havaw uses the % 2 = = 0 { means the number will be divide by 2 and equal to 0 }
+    
